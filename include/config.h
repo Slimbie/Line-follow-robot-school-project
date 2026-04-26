9e7592706655f69ea48e37bd4da631d96b5e046b
Slimbie <meijsonny@gmail.com>
2026-04-26 20:11:35 +0200

    refactor: Clean up config and sensors headers for improved readability and organization

--- include/config.h
+++ include/config.h
@@ -4,84 +4,62 @@
 
 #include <Arduino.h>
 
+// --- Start/Finish & Logica ---
+#define USE_START_FINISH      true
+#define FINISH_DRIVE_ON_CM    10.0   // Hoeveel cm doorrijden na detectie balk
+#define SAVE_POINT_INTERVAL   5.0    // Interval voor opslaan mapping punten
+#define MID_SENSOR_MASK       0b00011000
+#define TICKS_PER_CM          58.0   // Aantal encoder ticks per centimeter
 
-// --- Start/Finish Instellingen ---
-#define USE_START_FINISH true
-#define FINISH_DRIVE_ON_CM 10.0 // Hoeveel cm doorrijden na detectie balk
+// --- Snelheden (0-255) ---
+#define BASE_SPEED_MAPPING    100    // Snelheid tijdens het mappen
+#define BASE_SPEED_TURNS      90     // Snelheid in bochten
+#define MAX_SPEED             180    // Maximale snelheid op rechte stukken
 
-// --- Logica Instellingen ---
-#define MID_SENSOR_MASK 0b00011000
-#define SAVE_POINT_INTERVAL 5.0
-
-// Corrigeer ook deze (hoofdletters maken):
-#define BASE_SPEED_TURNS 100
-
-// Voeg dit toe aan Config.h
-#define TICKS_PER_CM 58.0 // Aantal encoder ticks per centimeter (afhankelijk van je wielen en encoders)
-
-// --- Knoppen & Switches ---
-#define START_BUTTON_PIN 23
-#define MODE_SWITCH_PIN 19
+// --- PID Waarden ---
+#define KP                    1.5    // Proportional
+#define KI                    0.0    // Integral
+#define KD                    5.0    // Derivative
 
 // --- IR Sensor Array ---
 const int SENSOR_COUNT = 8;
 const int sensorPinnen[SENSOR_COUNT] = {32, 33, 34, 35, 36, 39, 25, 26}; 
-#define IR_THRESHOLD 4050 
+#define IR_THRESHOLD          4050 
 
-// In Config.h
-// PID Waarden (Beginwaarden, moeten getuned worden)
-#define KP 2.3 
-#define KI 0.0
-#define KD 16.0
-//Kp = 2.3, Ki = 0.00, Kd = 16.0; 
-
-#define BASE_SPEED_MAPPING 60 // Rustige snelheid voor het mappen
-#define BASE_SPEED_turns 100 // Rustige snelheid voor het mappen
-#define MAX_SPEED 180 //kan tot 255, maar 200 is vaak al snel genoeg
+// --- Knoppen & Switches ---
+#define START_BUTTON_PIN      23
+#define MODE_SWITCH_PIN       19
+#define BUTTON_PIN            0      // Extra knop (bijv. BOOT knop ESP32)
 
 // --- Ultrasoon ---
-#define TRIG_PIN 5
-#define ECHO_PIN 18
-#define DISTANCE_THRESHOLD 5.0 // cm
+#define TRIG_PIN              5
+#define ECHO_PIN              18
+#define DISTANCE_THRESHOLD    5.0    // Detectie afstand in cm
 
 // --- Motoren (L298N / OT2065) ---
 // Links
-#define MOT_L_PWM 27
-#define MOT_L_AIN1 14
-#define MOT_L_AIN2 4
+#define MOT_L_PWM             27
+#define MOT_L_AIN1            14
+#define MOT_L_AIN2            4
 // Rechts
-#define MOT_R_PWM 22
-#define MOT_R_BIN1 21
-#define MOT_R_BIN2 16
+#define MOT_R_PWM             22
+#define MOT_R_BIN1            21
+#define MOT_R_BIN2            16
 
 // PWM Kanalen (ESP32 specifiek)
-#define PWM_FREQ 20000
-#define PWM_RES 8
-#define CH_LEFT 0
-#define CH_RIGHT 1
+#define PWM_FREQ              20000
+#define PWM_RES               8
+#define CH_LEFT               0
+#define CH_RIGHT              1
 
-// --- Motor Richting ---
-#define LEFT_MOTOR_REVERSED true  // Deze motor zit fysiek 180 graden gedraaid
-#define RIGHT_MOTOR_REVERSED false
+// Motor Richting
+#define LEFT_MOTOR_REVERSED   true   // Fysiek 180 graden gedraaid
+#define RIGHT_MOTOR_REVERSED  false
 
 // --- Encoders ---
-#define ENC_L_A 12
-#define ENC_L_B 13
-#define ENC_R_A 15
-#define ENC_R_B 2
-
-// --- Overige ---
-//kijk na of de volgende dubbel is met de motor pinnen, anders aanpassen
-
-#define BASE_SPEED_MAPPING 100
-#define MAX_SPEED 180
-#define BASE_SPEED_TURNS 90
-#define MID_SENSOR_MASK 0b00011000
-#define KP 1.5   // Jouw PID waardes
-#define KD 5.0   // Jouw PID waardes
-#define USE_START_FINISH true
-#define FINISH_DRIVE_ON_CM 10.0
-#define SAVE_POINT_INTERVAL 5.0
-#define BUTTON_PIN 0 // Of de pin die je gebruikt
+#define ENC_L_A               12
+#define ENC_L_B               13
+#define ENC_R_A               15
+#define ENC_R_B               2
 
 #endif
--- include/sensors.h
+++ include/sensors.h
@@ -2,23 +2,15 @@
 #define SENSORS_H
 
 #include <Arduino.h>
-#include <ESP32Encoder.h> // Zorg dat deze hier ook staat voor het type
+#include "types.h" // Haalt SensorData hier vandaan
 
-struct SensorData {
-    uint8_t sensorMask;
-    float linePosition;
-    bool lineDetected;
-    float distance;         // Ultrasoon
-    float distanceDriven;   // Encoders in cm
-    int32_t leftTicks;
-    int32_t rightTicks;
-    int irValues[8];
-};
-
+// --- EXTERN GEGEVENS ---
+// Hiermee weten andere bestanden dat deze variabelen elders (in sensors.cpp) bestaan
 extern volatile SensorData currentSensors;
-extern ESP32Encoder encL; // <--- VOEG DIT TOE
-extern ESP32Encoder encR; // <--- VOEG DIT TOE
+extern const int sensorPinnen[8];
 
+// --- FUNCTIES ---
 void startSensorTask();
+float readUltrasonic();
 
 #endif
